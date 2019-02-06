7df459d9c48ba6ea44c57a77431e15ec2d77ae49
Marshal1996 <453607106@qq.com>
2019-02-07 00:45:24 +0800

    [add]-新增返回数据工具类

---
+++ halcyon-core/src/main/java/com/marshal/halcyon/core/util/ResponseUtils.java
@@ -0,0 +1,27 @@
+package com.marshal.halcyon.core.util;
+
+import com.marshal.halcyon.core.component.ResponseData;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @auth: Marshal
+ * @date: 2019/2/6
+ * @desc: 通用返回数据工具
+ */
+public class ResponseUtils {
+
+    private static final String DEFAULT_RESPONSE_SUCCESS_MSG = "操作成功!";
+
+    private static final String DEFAULT_RESPONSE_FAIL_MSG = "操作失败!";
+
+
+    public static ResponseData responseOk(String msg) {
+        ResponseData responseData = new ResponseData(true, StringUtils.isNotEmpty(msg) ? msg : DEFAULT_RESPONSE_SUCCESS_MSG);
+        return responseData;
+    }
+
+    public static ResponseData responseErr(String msg) {
+        ResponseData responseData = new ResponseData(false, StringUtils.isNotEmpty(msg) ? msg : DEFAULT_RESPONSE_FAIL_MSG);
+        return responseData;
+    }
+}
