63fc2527186decd3f3b57463e89e762feec05544
Marshal1996 <453607106@qq.com>
2019-03-20 00:08:51 +0800

    [update]-调整模块架构

---
+++ halcyon-base/src/main/java/com/marshal/halcyon/base/hr/service/impl/HrEmployeeServiceImpl.java
@@ -0,0 +1,28 @@
+package com.marshal.halcyon.base.hr.service.impl;
+
+import com.marshal.halcyon.core.service.impl.BaseServiceImpl;
+import com.marshal.halcyon.base.hr.mapper.HrEmployeeMapper;
+import com.marshal.halcyon.base.hr.service.HrEmployeeService;
+import com.marshal.halcyon.base.hr.entity.HrEmployee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class HrEmployeeServiceImpl extends BaseServiceImpl<HrEmployee> implements HrEmployeeService {
+
+    @Autowired
+    HrEmployeeMapper hrEmployeeMapper;
+
+    @Override
+    public HrEmployee selectByUserId(Long userId) {
+        return hrEmployeeMapper.getEmployeeByUserId(userId);
+    }
+
+    @Override
+    public List<Map> getEmpOptions() {
+        return hrEmployeeMapper.getEmpOptions();
+    }
+}
