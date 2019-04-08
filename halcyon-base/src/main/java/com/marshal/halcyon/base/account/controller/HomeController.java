6197cedd23dc8f3c3be4448cb6a88c370619fab2
Marshal1996 <453607106@qq.com>
2019-04-08 11:22:16 +0800

    [update]-更新首页样式

---
+++ halcyon-base/src/main/java/com/marshal/halcyon/base/account/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.marshal.halcyon.base.account.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @auth: Marshal
+ * @date: 2019/4/7
+ * @desc: 主页controller
+ */
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+}
--- halcyon-web/src/main/resources/static/js/index.js
+++ halcyon-web/src/main/resources/static/js/index.js
@@ -1,40 +1,59 @@
-$(document).ready(function () {
-    $.ajax({
-        type: "GET",
-        url: "sys/function/getMenus",
-        contentType: 'application/x-www-form-urlencoded;charset=utf-8',
-        dataType: "text",
-        success: function (responseData) {
-            var funtionTree = JSON.parse(responseData);
-            var menuHtml = '';
-            menuHtml += parseToMenu(funtionTree);
-            $('.sidebar-menu').append(menuHtml);
-        },
-        error: function (e) {
-            console.log(e);
-        }
-    });
-
-    function parseToMenu(functionTree) {
-        var menuHtml = "";
-        functionTree.forEach(function (treeNode) {
-            if (treeNode.childFunctions == undefined || treeNode.childFunctions == null) {
-                menuHtml += '<li>\n' +
-                    '    <a href="javascript:$(\'#mainframe\').attr(\'src\',\'' + treeNode.url + '\')"><i class="' + treeNode.functionIcon + '"></i> <span>' + treeNode.functionName + '</span></a>\n' +
-                    '</li>';
-            } else if (treeNode.childFunctions != undefined) {
-                menuHtml += '<li class="treeview">\n' +
-                    '            <a href="#"><i class="' + treeNode.functionIcon + '"></i> <span>' + treeNode.functionName + '</span>\n' +
-                    '                <span class="pull-right-container">\n' +
-                    '                            <i class="fa fa-angle-left pull-right"></i>\n' +
-                    '                        </span>\n' +
-                    '            </a>\n' +
-                    '            <ul class="treeview-menu">';
-                menuHtml += parseToMenu(treeNode.childFunctions);
-                menuHtml += '</ul>\n' +
-                    '        </li>';
-            }
-        });
-        return menuHtml;
+/**
+ * index.js
+ * @author:Marshal
+ */
+$.ajax({
+    type: "GET",
+    url: "sys/function/getMenus",
+    contentType: 'application/x-www-form-urlencoded;charset=utf-8',
+    dataType: "text",
+    success: function (responseData) {
+        var funtionTree = JSON.parse(responseData);
+        var menuHtml = '';
+        menuHtml += parseToMenu(funtionTree);
+        $('.sidebar-menu').append(menuHtml);
+    },
+    error: function (e) {
+        console.log(e);
     }
 });
+
+function parseToMenu(functionTree) {
+    var menuHtml = "";
+    functionTree.forEach(function (treeNode) {
+        if (treeNode.childFunctions == undefined || treeNode.childFunctions == null) {
+            menuHtml += '<li onclick="jumpToFunction(\'' + treeNode.functionName + '\',\'' + treeNode.url + '\')">\n' +
+                '    <a><i class="' + treeNode.functionIcon + '"></i> <span>' + treeNode.functionName + '</span></a>\n' +
+                '</li>';
+        } else if (treeNode.childFunctions != undefined) {
+            menuHtml += '<li class="treeview">\n' +
+                '            <a href="#"><i class="' + treeNode.functionIcon + '"></i> <span>' + treeNode.functionName + '</span>\n' +
+                '                <span class="pull-right-container">\n' +
+                '                            <i class="fa fa-angle-left pull-right"></i>\n' +
+                '                        </span>\n' +
+                '            </a>\n' +
+                '            <ul class="treeview-menu">';
+            menuHtml += parseToMenu(treeNode.childFunctions);
+            menuHtml += '</ul>\n' +
+                '        </li>';
+        }
+    });
+    return menuHtml;
+}
+
+function jumpToFunction(functionName, functionUrl) {
+    $('#bread-function').text(functionName);
+    $('#bread-function').css('display', 'inline');
+    $('#mainframe').attr('src', functionUrl);
+}
+
+function backToIndex() {
+    $('#bread-function').text('');
+    $('#bread-function').css('display', 'none');
+    $('#mainframe').attr('src', 'dashboard.html');
+}
+
+function refreshFunction() {
+    var mainframe = window.document.getElementById('mainframe');
+    mainframe.contentWindow.location.reload(true);
+}
